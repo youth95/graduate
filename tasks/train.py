9f8bf78cca07c3ceb38e38d18a8e871dc4d389a1
赵前 <zhaoqian.ipp@bytedance.com>
2020-12-10 14:12:17 +0800

    fix: change

--- tasks/train.py
+++ tasks/train.py
@@ -10,57 +10,44 @@ import pandas as pd
 import torch.nn as nn
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
-import time
 import torch
+import numpy as np
+import math
+import time
+
 import utils.evaluate_function as ef
 
 start = time.time()
+data = pd.read_csv('../data/72_predicted_72_supervised_data.csv')
 
-"""
-可调整参数
-"""
-
-lr = 1e-4
-epoches = 1000
+lr = 1e-3
+epoches = 100
 criterion = nn.MSELoss()
 weight_decays = 1e-3
 batch_sizes = 64
-in_n_features = 6
-out_n_features = 2
-
-in_seq_len = 16
-out_seq_len = 8
-num_layers = 3
-embedding_dim = 64
-
-"""
-数据处理
-"""
-data = pd.read_csv('../data/48_predicted_24_supervised_data.csv').fillna(0.0)
-data_new = data.iloc[16:, 1:]
+data_new = data.iloc[:, 1:]
+print(data_new.shape)
 data_train, data_test = train_test_split(data_new, test_size=0.2, shuffle=True)
-train_data = DataSet_seq(data_train)
-test_data = DataSet_seq(data_test)
+train_data = DataSet_seq(data_train, 24)
+test_data = DataSet_seq(data_test, 24)
+
 train_dataloader = DataLoader(dataset=train_data, batch_size=batch_sizes, shuffle=True)
 test_dataloader = DataLoader(dataset=test_data, batch_size=batch_sizes, shuffle=False)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-model = Seq2Seq(
-    in_seq_len=in_seq_len,
-    in_n_features=in_n_features,
-    out_n_features=out_n_features,
-    num_layers=num_layers,
-    embedding_dim=embedding_dim,
-    out_seq_len=out_seq_len
-).to(device)
 
 
 def init_weights(m):
     for name, parm in m.named_parameters():
         #         print(name,parm)
-        nn.init.uniform_(parm.data, -0.07, 0.07)
+        nn.init.uniform_(parm.data, -0.08, 0.08)
 
 
+n_features = 6
+seq_length = 24
+label_length = 24
+
+model = Seq2Seq(seq_len=24, n_features=6, embedding_dim=128, output_length=24)
+# init_weights(model)
 model.apply(init_weights)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decays)
 
@@ -73,8 +60,8 @@ if __name__ == '__main__':
         t = tqdm(train_dataloader, leave=False, total=len(train_dataloader))
         for batch_idx, data in enumerate(t):
             x, y = data
-            x = x.to(device).view(-1, in_seq_len, in_n_features)
-            y = y.to(device).view(-1, out_seq_len, out_n_features)
+            x = x.to(device).view(-1, seq_length, n_features)
+            y = y.to(device).view(-1, label_length, 2)
             optimizer.zero_grad()
             output = model(x)
             loss = criterion(output.permute(1, 0, 2), y)
@@ -87,22 +74,24 @@ if __name__ == '__main__':
             optimizer.step()
             total_loss += loss.cpu().item()
         test_loss_one, eval_list = ef.evaulate(model, test_dataloader, criterion)
-        print('epoch:{},tr_l:{},te_l:{},test_mae：{}, test_mse:{}, test_rmse:{},R2_lat:{},R2_lon:{},E1:{},E2:{}'.format(
-            epoch + 1,
-            total_loss / len(train_dataloader),
-            test_loss_one,
-            eval_list[0],
-            eval_list[1],
-            eval_list[2],
-            eval_list[3],
-            eval_list[4],
-            eval_list[5],
-            eval_list[6],
-        ))
+        print(
+            'epoch:{},训练集 loss:{}, 测试集 loss:{}, 反归一化后 结果 测试 MAE：{}, MSE:{}, RMSE:{},R2_lat:{},R2_lon:{},APE:{}'.format(
+                epoch + 1,
+                total_loss / len(train_dataloader),
+                test_loss_one,
+                eval_list[0],
+                eval_list[1],
+                eval_list[2],
+                eval_list[3],
+                eval_list[4],
+                eval_list[5]
+            ))
         test_loss.append(test_loss_one)
         train_loss.append(total_loss / len(train_dataloader))
     # 可视化
     ef.loss_show(test_loss, train_loss, epoches)
     ef.save_model(model)
+print(test_loss)
+print(train_loss)
 end = time.time()
 print('总时间time：', (end - start))
--- utils/evaluate_function.py
+++ utils/evaluate_function.py
@@ -6,143 +6,45 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
+from torch.utils.data import Dataset
+class DataSet(Dataset):
+
+    def __init__(self, data_pd,step):
+        data = np.array(data_pd)
+        self.x_data = torch.from_numpy(data[:, 0:step * 6]).type(torch.float32)
+        self.y_data = torch.from_numpy(data[:, step * 6:]).type(torch.float32)
+        self.len = data.shape[0]
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.len
+
 
 n_features = 6
-seq_length = 16
-label_length = 8
+seq_length = 24
+label_length = 24
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-def unnormal(data):
-    '''
-    :param data: 归一化后的数据
-    :return: 反归一化
-    LAT_MAX = 179.96099999999998 LAT_MIN = 100.0
-    LON_MAX = 62.1 LON_MAX = 0.6
-    '''
-    LAT_MAX = 179.96099999999998
-    LAT_MIN = 100.0
-    LON_MAX = 62.1
-    LON_MIN = 0.6
-    data[:, :, 0:1] = data[:, :, 0:1] * (LAT_MAX - LAT_MIN) + LAT_MAX
-    data[:, :, 1:] = data[:, :, 1:] * (LON_MAX - LON_MIN) + LON_MIN
-    return data
-
-
-# if __name__ == '__main__':
-#     a = torch.randn((1, 8, 2))
-#     print(a)
-#     data1 = unnormal(a)
-#     print(data1.shape)
-#     print(data1)
-
-
-def mae_mse_rmse(target, prediction):
-    '''
-    :param target: (batch,label_len,features)
-    :param prediction:
-    :return:
-    '''
-    # print('target shape', target.shape)
-    n = len(target) * 2
-    # 反归一化后的数据
-    # un_target = unnormal(target)
-    # un_pred = unnormal(prediction)
-
-    lat_real = np.array(target[:, :, 0:1].squeeze(-1).cpu())
-    lon_real = np.array(target[:, :, 1:].squeeze(-1).cpu())
-    lat_pred = np.array(prediction[:, :, 0:1].squeeze(-1).cpu())
-    lon_pred = np.array(prediction[:, :, 1:].squeeze(-1).cpu())
-    lat_mae = mean_absolute_error(lat_pred, lat_real)
-    lon_mae = mean_absolute_error(lon_pred, lon_real)
-    mae = (lat_mae + lon_mae) / 2
-    lat_mse = mean_squared_error(lat_pred, lat_real)
-    lon_mse = mean_squared_error(lon_pred, lon_real)
-    mse = (lat_mse + lon_mse) / 2
-    lat_r2 = r2_score(lat_pred, lat_real)
-    lon_r2 = r2_score(lon_pred, lon_real)
-    rmse = mse ** 0.5
-    return mae, mse, rmse, lat_r2, lon_r2
-
-    # err1_mae = torch.abs(target[:,:, 0:1] - prediction[:,:, 0:1])
-    # err2_mae = torch.abs(target[:, :,1:] - prediction[:, :,1:])
-    # mae = (torch.sum(err1_mae).item() + torch.sum(err2_mae).item()) / n
-    # mse = (torch.sum(err1_mae * err1_mae).item() + torch.sum(err2_mae * err2_mae).item()) / n
-    # rmse = math.sqrt(mse)
-    # # 经度的R2，维度的R2
-    # R2_lat = R(target[:,:, 0:1], prediction[:, :,0:1])
-    # R2_lon = R(target[:,:, 1:], prediction[:,:, 1:])
-    # return mae, mse, rmse, R2_lat, R2_lon
-
-
-def evaulate(model, test_dataloader, loss_func):
-    model.eval()
-    # it = iter(test_dataloader)
-    total_count = 0
-    total_loss = 0
-    total_mae = 0
-    total_mse = 0
-    total_rmse = 0
-    total_R2_lat = 0
-    total_R2_lon = 0
-    with torch.no_grad():
-        for i, data in enumerate(test_dataloader):
-            x, y = data
-            x = x.to(device).view(-1, seq_length, n_features)
-            y = y.to(device).view(-1, label_length, 2)
-            output_test = model(x)
-            output_test = output_test.permute(1, 0, 2)
-            with torch.no_grad():
-                loss = loss_func(output_test, y)
-                mae, mse, rmse, R2_lat, R2_lon = mae_mse_rmse(output_test, y)
-                E1, E2 = distance(output_test, y)  # 运算经纬度误差 返回 E1 E2
-                total_count += 1
-                total_loss += loss.item()
-                total_mae += mae
-                total_mse += mse
-                total_rmse += rmse
-                total_R2_lat += R2_lat
-                total_R2_lon += R2_lon
-    model.train()
-    return total_loss / total_count, [
-        total_mae / total_count,
-        total_mse / total_count,
-        total_rmse / total_count,
-        total_R2_lat / total_count,
-        total_R2_lon / total_count,
-        E1,
-        E2,
-    ]
-
-
 def to_radians(x, *y):
     return math.radians(x)
 
-
-def distance(prediction, target):
+def distance(pred, real):
     '''
     :param pred:  shape (n,2)
     :param real:  shape (n,2)
     :return: (n,1)
     '''
     R = 6357
-    # 反归一化后的数据
-    real = unnormal(target)
-    pred = unnormal(prediction)
-    real = real.reshape((-1, 2))
-    pred = pred.reshape((-1, 2))
-    latPred = pred[:, 0].cpu()
-    latPred.map_(latPred, to_radians)
-    lonPred = pred[:, 1].cpu()
-    lonPred.map_(lonPred, to_radians)
-    latReal = real[:, 0].cpu()
-    latReal.map_(latReal, to_radians)
-    lonReal = real[:, 1].cpu()
-    lonReal.map_(lonReal, to_radians)
-    # print(latPred)
-    # print(lonPred)
-    # print(latReal)
-    # print(lonReal)
+    latPred = pred[:,:, 0:1].squeeze(-1)
+    latPred.map_(latPred,to_radians)
+    lonPred = pred[:,: ,1:].squeeze(-1).cpu()
+    lonPred.map_(lonPred,to_radians)
+    latReal = real[:,:, 0:1].squeeze(-1).cpu()
+    latReal.map_(latPred,to_radians)
+    lonReal = real[:,:, 1:].squeeze(-1).cpu()
+    lonReal.map_(lonReal,to_radians)
+
     E1 = 2 * R * torch.asin(
         torch.sqrt(
             torch.sin(
@@ -159,7 +61,97 @@ def distance(prediction, target):
         )
     )
 
-    return np.average(E1), np.average(E2)
+    return torch.mean(E1)
+
+
+def unnormal(data):
+    '''
+    :param data: 归一化后的数据
+    :return: 反归一化
+    LAT_MAX = 179.96099999999998 LAT_MIN = 100.0
+    LON_MAX = 62.1 LON_MAX = 0.6
+    '''
+    LAT_MAX = 179.96099999999998
+    LAT_MIN = 100.0
+    LON_MAX = 62.1
+    LON_MIN = 0.6
+    data[:, :, 0:1] = data[:, :, 0:1] * (LAT_MAX - LAT_MIN) + LAT_MIN
+    data[:, :, 1:] = data[:, :, 1:] * (LON_MAX - LON_MIN) + LON_MIN
+    return data
+
+
+def mae_mse_rmse(target, prediction):
+    '''
+    :param target: (batch,label_len,features)
+    :param prediction:
+    :return:
+    '''
+    target = target.cpu()
+    prediction = prediction.cpu()
+    # print('target shape', target.shape)
+    n = len(target) * 2
+    # 反归一化后的数据
+    target = unnormal(target)
+    prediction = unnormal(prediction)
+    # print('反归一化后的target',target[0:1,])
+    # print('反归一化后的目标prediction',prediction[0:1,])
+    E1 = distance(target,prediction)
+
+    lat_real = np.array(target[:, :, 0:1].squeeze(-1))
+    lon_real = np.array(target[:, :, 1:].squeeze(-1))
+    lat_pred = np.array(prediction[:, :, 0:1].squeeze(-1))
+    lon_pred = np.array(prediction[:, :, 1:].squeeze(-1))
+    lat_mae = mean_absolute_error(lat_pred, lat_real)
+    lon_mae = mean_absolute_error(lon_pred, lon_real)
+    mae = (lat_mae + lon_mae) / 2
+    lat_mse = mean_squared_error(lat_pred, lat_real)
+    lon_mse = mean_squared_error(lon_pred, lon_real)
+    mse = (lat_mse + lon_mse) / 2
+    lat_r2 = r2_score(lat_pred, lat_real)
+    lon_r2 = r2_score(lon_pred, lon_real)
+    rmse = mse ** 0.5
+    return mae, mse, rmse, lat_r2, lon_r2,E1
+
+def evaulate(model, test_dataloader, loss_func):
+    model.eval()
+    # it = iter(test_dataloader)
+    total_count = 0
+    total_loss = 0
+    total_mae = 0
+    total_mse = 0
+    total_rmse = 0
+    total_R2_lat = 0
+    total_R2_lon = 0
+    total_E1 = 0
+    with torch.no_grad():
+        for i, data in enumerate(test_dataloader):
+            x, y = data
+            x = x.to(device).view(-1, seq_length, n_features)
+            y = y.to(device).view(-1, label_length, 2)
+            output_test = model(x)
+            # print('测试集输出 output_test',output_test)
+            # rnn测试不需要,但是seq2seq需要
+            output_test = output_test.permute(1, 0, 2)
+            with torch.no_grad():
+                loss = loss_func(output_test, y)
+                mae, mse, rmse, R2_lat, R2_lon,E1 = mae_mse_rmse(output_test, y)
+                total_count += 1
+                total_loss += loss.item()
+                total_mae += mae
+                total_mse += mse
+                total_rmse += rmse
+                total_R2_lat += R2_lat
+                total_R2_lon += R2_lon
+                total_E1 += E1
+    model.train()
+    return total_loss / total_count, [
+        total_mae / total_count,
+        total_mse / total_count,
+        total_rmse / total_count,
+        total_R2_lat / total_count,
+        total_R2_lon / total_count,
+        total_E1 / total_count
+    ]
 
 
 def loss_show(test_loss, train_loss, epoches):
@@ -174,7 +166,7 @@ def loss_show(test_loss, train_loss, epoches):
 
 
 def save_model(model):
-    torch.save(model, "./model.pt")
+    torch.save(model, "./72_72_Seq2Seq_att_model.pkl")
 
 #
 # if __name__ == '__main__':
