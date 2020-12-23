1ee7a3dac03185a3fe55061b37fec122932462b8
董萍萍 <dongpingping@dongpingpingdeMacBook-Pro.local>
2020-12-23 23:20:17 +0800

    fix:评估

---
+++ tasks/evaluate.py
@@ -0,0 +1,66 @@
+import torch
+import os, sys
+
+os.chdir(os.path.dirname(__file__))
+sys.path.append("..")
+from torch import nn
+from env.base import *
+from models.layers import ModelDriver
+from data_loaders.tmp_sat import fetch_data_set, MockDataSet
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import tasks.assessment as assessment
+
+lr = 1e-3
+epoches = 100
+weight_decays = 1e-3
+batch_sizes = 2
+
+criterion = nn.MSELoss(size_average=False)
+
+data_set = fetch_data_set([2010])
+all_length = len(data_set)
+train_size = int(all_length * 0.8)
+test_size = all_length - train_size
+train_data, test_data = torch.utils.data.random_split(data_set, [train_size, test_size])
+test_dataloader = DataLoader(dataset=test_data, batch_size=batch_sizes, shuffle=False)
+
+model = ModelDriver().todevice()
+model = torch.load('../model_file/48_24_MAR_ConvLSTM.pkl')
+
+# 评估
+test_set = tqdm(test_dataloader, leave=False, total=len(test_dataloader))
+
+model.eval()
+total_test_loss = 0
+test_count = 0
+mae, rmse, mse, E1, lat_r2, lon_r2 = 0, 0, 0, 0, 0, 0
+test_loss = []
+for batch_idx, data in enumerate(test_set):
+    if test_count == 3:
+        break
+    test_count += 1
+    x, y = data
+    x = x.to(device).type(torch.float32)
+    y = y.to(device)
+    output = model(x)
+    loss = criterion(output, y)
+    loss_aver = loss.item()
+    test_set.set_postfix({
+        'testloss': '{:.8f}'.format(loss_aver),
+    })
+    total_test_loss += loss_aver
+    # 评估其他指标
+    avg_mae, avg_mse, each_rmse, each_lat_r2, each_lon_r2, each_E1 = assessment.evaluate(output, y)
+    mae += avg_mse
+    mse += avg_mse
+    rmse += each_rmse
+    lat_r2 += each_lat_r2
+    lon_r2 += each_lon_r2
+    E1 += each_E1
+avg_test_loss = total_test_loss / test_count
+test_loss.append(avg_test_loss)
+print('测试集 loss:{},测试集的mae:{},mse:{},rmse:{},APE:{},lat_r2:{},lon_r2:{}'.format(
+    avg_test_loss, mae / test_count, mse / test_count, rmse / test_count, E1 / test_count, lat_r2 / test_count,
+                   lon_r2 / test_count
+))
--- tasks/image_train.py
+++ tasks/image_train.py
@@ -16,9 +16,9 @@ epoches = 100
 weight_decays = 1e-3
 batch_sizes = 2
 
-criterion = nn.MSELoss(size_average=False)
+criterion = nn.MSELoss()
 
-data_set = fetch_data_set([2010, 2011])
+data_set = fetch_data_set([2010])
 all_length = len(data_set)
 train_size = int(all_length * 0.8)
 test_size = all_length - train_size
@@ -39,14 +39,16 @@ def init_weights(m):
 optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decays)
 
 if __name__ == "__main__":
-    train_loss = []
+    train_loss, test_loss = [], []
     for epoch in range(epoches):
         model = model.train()
-        total_loss = 0
-        count = 0
-        t = tqdm(train_dataloader, leave=False, total=len(train_dataloader))
-        for batch_idx, data in enumerate(t):
-            count += 1
+        total_train_loss, total_test_loss = 0, 0
+        train_count, test_count = 0, 0
+        mae, mse, rmse, lat_r2, lon_r2, E1 = 0, 0, 0, 0, 0, 0
+        train_set = tqdm(train_dataloader, leave=False, total=len(train_dataloader))
+        model.train()
+        for batch_idx, data in enumerate(train_set):
+            train_count += 1
             x, y = data
             x = x.to(device).type(torch.float32)
             y = y.to(device)
@@ -54,33 +56,50 @@ if __name__ == "__main__":
             output = model(x)
             loss = criterion(output, y)
             loss_aver = loss.item()
-            t.set_postfix({
+            train_set.set_postfix({
                 'trainloss': '{:.8f}'.format(loss_aver),
                 'epoch': '{:02d}'.format(epoch)
             })
             loss.backward()
             optimizer.step()
-            total_loss += loss_aver
-            # test_loss_one, eval_list = ef.evaulate(model, test_dataloader, criterion)
-            # print(
-            #     'epoch:{},训练集 loss:{}, 测试集 loss:{}, 反归一化后 结果 测试 MAE：{}, MSE:{}, RMSE:{},R2_lat:{},R2_lon:{},APE:{}'.format(
-            #         epoch + 1,
-            #         total_loss / len(train_dataloader),
-            #         test_loss_one,
-            #         eval_list[0],
-            #         eval_list[1],
-            #         eval_list[2],
-            #         eval_list[3],
-            #         eval_list[4],
-            #         eval_list[5]
-            #     ))
-            # lat,lon = assessment.graph_to_lon_lat(output)
-            # print(lat,lon)
-        # 每一个epoches的loss
+            total_train_loss += loss_aver
+        avg_train_loss = total_train_loss / train_count
+        train_loss.append(avg_train_loss)
+        # 评估
+        test_set = tqdm(test_dataloader, leave=False, total=len(test_dataloader))
 
-        train_loss.append(total_loss / count)
-        print('train_loss', train_loss)
-        print('总共loss：', total_loss / count)
-
-    assessment.loss_show(train_loss, epoches)
+        model.eval()
+        for batch_idx, data in enumerate(test_set):
+            test_count += 1
+            x, y = data
+            x = x.to(device).type(torch.float32)
+            y = y.to(device)
+            output = model(x)
+            loss = criterion(output, y)
+            loss_aver = loss.item()
+            test_set.set_postfix({
+                'testloss': '{:.8f}'.format(loss_aver),
+                'epoch': '{:02d}'.format(epoch)
+            })
+            total_test_loss += loss_aver
+            # 评估其他指标
+        #     avg_mae, avg_mse, each_rmse, each_lat_r2, each_lon_r2, each_E1 = assessment.evaluate(output, y)
+        #     mae += avg_mse
+        #     mse += avg_mse
+        #     rmse += each_rmse
+        #     lat_r2 += each_lat_r2
+        #     lon_r2 += each_lon_r2
+        #     E1 += each_E1
+        avg_test_loss = total_test_loss / test_count
+        test_loss.append(avg_test_loss)
+        # print('Epoch：{},训练集loss:{},测试集loss:{}\t测试集的mae:{},mse:{},rmse:{},APE:{},lat_r2:{},lon_r2:{}'.format(epoch,
+        #                                                                                                    avg_train_loss,
+        #                                                                                                    avg_test_loss,
+        #                                                                                                    mae / test_count,
+        #                                                                                                    mse / test_count,
+        #                                                                                                    rmse / test_count,
+        #                                                                                                    E1 / test_count,
+        #                                                                                                    lat_r2 / test_count,
+        print('Epoch：{},训练集loss:{},测试集loss:{}'.format(epoch, avg_train_loss, avg_test_loss))
+    assessment.loss_show(train_loss, test_loss, epoches)
     torch.save(model, '48_24_MAR_ConvLSTM.pkl')
